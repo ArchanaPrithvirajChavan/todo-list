1a480a99455bd8d9025624287e66b96bff145660
Archana Chavan <archana.chavan2@gmail.com>
2025-11-25 14:57:26 -0800

    TodoForm is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import './App.css'
+import TodoList from './TodoList.jsx';
+import TodoForm from './TodoForm.jsx';
 
 function App() {
-const todos = [
-    {id: 1, title: "review resources"},
-    {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
-]
-return (
-    <div>
-        <h1>MY Todo List</h1>
-        <ul>
-            {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
-        </ul>
-    </div>
+  return (
+   <>
+    <h1>MY Todo List</h1>
+    <TodoForm/> 
+      <TodoList />
+    </>
   );
 }
 
-export default App
+export default App;
