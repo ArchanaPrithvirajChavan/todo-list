d479f6b5507c9727bfcef5337ae7e65ba4c01ade
Archana Chavan <archana.chavan2@gmail.com>
2025-12-08 15:25:10 -0800

    week-03 components and state added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import './App.css';
+import React, { useState } from 'react';
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
 function App() {
+  const [newTodo, setNewTodo] = useState("Hello");
+
   return (
-   <>
-    <h1>MY Todo List</h1>
-    <TodoForm/> 
+    <>
+      <h1>MY Todo List</h1>
+
+      <TodoForm />
+
+    
+      <p>{newTodo}</p>
+
       <TodoList />
     </>
   );
