e9788eefd5aff4d7fffcec9e8636a2c14d6b4b2b
Archana Chavan <archana.chavan2@gmail.com>
2026-02-16 17:31:06 -0800

    TodosPages.jsx created

---
+++ src/shared/Header.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+
+function Header({ title }) {
+  return (
+    <header>
+      <h1 className={styles.headerTitle}>{title}</h1>
+
+      <nav className={styles.nav}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.active : styles.inactive)}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? styles.active : styles.inactive)}
+        >
+          About
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
